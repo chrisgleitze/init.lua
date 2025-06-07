4ca0395d44cd6742c633a30db6db6f017d58ad4b
Christian Gleitze <connectingdotscoding@gmail.com>
2025-06-07 21:26:40 +0200

    colorscheme change

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,12 +4,6 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        require("github-theme").setup({
-            palettes = {
-                bg1 = "#FF0000",
-            },
-        })
-
         vim.cmd("colorscheme github_dark_default")
     end,
 }
