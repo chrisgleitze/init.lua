1e718a3524fdb0d8a6f3f17b6ece4d38036c51b6
Christian Gleitze <connectingdotscoding@gmail.com>
2025-07-08 12:41:39 +0200

    add formatter - conform.lua specs

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,25 @@
+return {
+    "stevearc/conform.nvim",
+    dependencies = { "mason.nvim" },
+    event = { "BufReadPre", "BufNewFile" },
+    lazy = false,
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+            typescript = { "prettier" },
+            javascriptreact = { "prettier" },
+            typescriptreact = { "prettier" },
+            css = { "prettier" },
+            html = { "prettier" },
+            json = { "rettier" },
+            yaml = { "prettier" },
+            markdown = { "prettier" },
+        },
+        format_on_save = {
+            lsp_fallback = true,
+            async = false,
+            timeout_ms = 1000,
+        },
+    },
+}
