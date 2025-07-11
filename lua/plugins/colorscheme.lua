e77f38765cdb64336e210066da0cebb218c0afcd
Christian Gleitze <connectingdotscoding@gmail.com>
2025-07-11 15:05:12 +0200

    edit colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,60 @@
 return {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
+
+    -- "projekt0n/github-nvim-theme",
+    -- name = "github-theme",
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --     vim.cmd("github_dark_default")
+    -- end,
+
+    -- "ellisonleao/gruvbox.nvim",
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --     vim.cmd("gruvbox")
+    -- end,
+
+    -- "rose-pine/neovim",
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --     require("rose-pine").setup({
+    --         disable_background = true,
+    --         styles = {
+    --             italic = false,
+    --         },
+    --     })
+    --
+    --     vim.cmd.colorscheme("rose-pine")
+    -- end,
+
+    "rose-pine/neovim",
+    name = "rose-pine",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd("colorscheme github_dark_default")
+        -- require("rose-pine").setup({
+        --     disable_background = true,
+        --     styles = {
+        --         italic = false,
+        --     },
+        -- })
+
+        vim.cmd.colorscheme("rose-pine")
     end,
+
+    -- "folke/tokyonight.nvim",
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --     vim.cmd.colorscheme("tokyonight-night")
+    -- end,
+
+    -- "marko-cerovac/material.nvim",
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --     vim.cmd.colorscheme("material")
+    -- end,
 }
