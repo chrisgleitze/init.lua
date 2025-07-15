634e24d4d04baf523ee3a7f4e545b35b5cf9d1aa
Christian Gleitze <connectingdotscoding@gmail.com>
2025-07-15 19:08:18 +0200

    add autoclose brackets plugin

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,19 @@
+return {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+
+    -- "m4xshen/autoclose.nvim",
+    -- config = function()
+    --     require("autoclose").setup()
+    -- end,
+
+    -- "altermo/ultimate-autopair.nvim",
+    -- event = { "InsertEnter", "CmdlineEnter" },
+    -- branch = "v0.6",
+    -- opts = {
+    --     --Config goes here
+    -- },
+}
