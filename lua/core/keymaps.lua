705d080cf1fc20d930474cbec374e91d1bfc695c
Christian Gleitze <connectingdotscoding@gmail.com>
2025-07-16 12:32:19 +0200

    change keymaps to move line up and down

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -29,13 +29,12 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- move lines up or down in normal, insert, and visual modes
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
-
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
-vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
-vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<C-j>", ":m .+1<CR>==")
+vim.keymap.set("n", "<C-k>", ":m .-2<CR>==")
+vim.keymap.set("i", "<C-j>", "<Esc>:m .+1<CR>==gi")
+vim.keymap.set("i", "<C-k>", "<Esc>:m .-2<CR>==gi")
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
 
 -- helps you change all occurrences of the word the cursor is on
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
