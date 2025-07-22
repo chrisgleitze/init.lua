b6fddd7437f275f25d0e4b22bf310114ab800ad0
Christian Gleitze <connectingdotscoding@gmail.com>
2025-07-22 06:44:52 +0200

    clean up lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,10 +2,8 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = {
         "nvim-tree/nvim-web-devicons",
-        "maxmx03/fluoromachine.nvim",
     },
     opts = {
-        theme = "fluoromachine",
         sections = {
             lualine_x = { "filetype" },
         },
