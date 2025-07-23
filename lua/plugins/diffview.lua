950267ac8d9b88c72e60ea495a42006d3fdccb4e
Christian Gleitze <connectingdotscoding@gmail.com>
2025-07-23 13:22:22 +0200

    add basic diffview config

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,14 +1,16 @@
 return {
     "sindrets/diffview.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    -- require("diffview").setup(),
-    config = function()
+    lazy = true,
+    keys = {
+        { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>" },
+        { "<leader>gB", "<cmd>DiffviewOpen origin/HEAD...HEAD --imply-local<cr>" },
+        -- keymap to quickly open and close diffview
         vim.keymap.set("n", "<leader>V", function()
             if next(require("diffview.lib").views) == nil then
                 vim.cmd("DiffviewOpen")
             else
                 vim.cmd("DiffviewClose")
             end
-        end)
-    end,
+        end),
+    },
 }
