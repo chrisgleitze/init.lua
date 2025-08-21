86a4fbcf158c2cf00d347912b1f75a3d6ea137d6
Christian Gleitze <connectingdotscoding@gmail.com>
2025-08-21 13:31:17 +0200

    blink: show documentation for autocomplete immediately

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -22,7 +22,7 @@ return {
             },
             documentation = {
                 auto_show = true,
-                -- auto_show_delay_ms = 200,
+                auto_show_delay_ms = 1,
                 window = {
                     border = "rounded",
                 },
