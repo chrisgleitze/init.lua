140ba2f8db4c3747b8e8924d87692d0b252f2498
Christian Gleitze <connectingdotscoding@gmail.com>
2025-09-21 20:16:26 +0200

    del laststatus, set syntax on

--- init.lua
+++ init.lua
@@ -2,8 +2,7 @@
 vim.g.mapleader = " "
 vim.o.undofile      = true
 vim.o.clipboard     = "unnamedplus"
-vim.o.laststatus    = 0
 vim.opt.expandtab   = true
 vim.opt.shiftwidth  = 4
 vim.opt.softtabstop = -1
-vim.cmd("syntax off | colorscheme retrobox | highlight Normal guifg=#ffaf00 guibg=#282828")
+vim.cmd("syntax on | colorscheme retrobox | highlight Normal guifg=#ffaf00 guibg=#282828")
