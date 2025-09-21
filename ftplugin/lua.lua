3304a0de5c63c0d77b2f15d29ab9cd142f0e5eb2
Christian Gleitze <connectingdotscoding@gmail.com>
2025-09-21 19:42:19 +0200

    del everything for most minimal Neovim config

--- ftplugin/lua.lua
+++
@@ -1,3 +0,0 @@
-local opt = vim.opt
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
-require("core.options")
-require("core.keymaps")
-require("core.lazy")
-
--- TODO
--- https://github.com/piersolenski/import.nvim
+-- minimal Neovim config
+vim.g.mapleader = " "
+vim.o.undofile      = true
+vim.o.clipboard     = "unnamedplus"
+vim.o.laststatus    = 0
+vim.opt.expandtab   = true
+vim.opt.shiftwidth  = 4
+vim.opt.softtabstop = -1
+vim.cmd("syntax off | colorscheme retrobox | highlight Normal guifg=#ffaf00 guibg=#282828")
