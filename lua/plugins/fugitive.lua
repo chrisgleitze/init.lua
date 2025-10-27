2b29cd5f10e5aa43dfebcc444bdfe83f58b07ed4
Christian Gleitze <connectingdotscoding@gmail.com>
2025-10-27 12:45:35 +0100

    add more fugitive keymaps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,7 +1,12 @@
 return {
     "tpope/vim-fugitive",
     config = function()
-        vim.keymap.set("n", "<leader>G", "<cmd>Git <CR>")
-        vim.keymap.set("n", "<leader>Gb", "<cmd>Git blame<CR>")
+        local map = vim.keymap.set
+        map("n", "<leader>Gi", "<cmd>Git <CR>")
+        map("n", "<leader>Gb", "<cmd>Git blame<CR>")
+        map("n", "<leader>Gl", "<cmd>Git log<CR>")
+        map("n", "<leader>Gs", "<cmd>Git status<CR>")
+        map("n", "<leader>Gd", "<cmd>Git diff<CR>")
+        map("n", "<leader>D", ":Git<CR><C-w><C-o>:bd<CR>")
     end,
 }
