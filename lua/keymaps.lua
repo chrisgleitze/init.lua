9b36b35137192220199fc5acb9dbc7931b5bb316
Christian Gleitze <connectingdotscoding@gmail.com>
2025-10-29 21:27:01 +0100

    refactor keymaps; add fugitive keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,48 +1,50 @@
+local map = vim.keymap.set
+
 -- open Lazy.nvim plugin manager
-vim.keymap.set("n", "<leader>L", "<cmd>Lazy<cr>")
+map("n", "<leader>L", "<cmd>Lazy<cr>")
 
 -- write buffer
-vim.keymap.set("n", "<leader>W", function()
+map("n", "<leader>W", function()
     vim.cmd("w")
     print("buffer saved")
 end)
 
 -- source buffer
-vim.keymap.set("n", "<leader>S", function()
+map("n", "<leader>S", function()
     vim.cmd("source %")
     print("buffer sourced")
 end)
 
 -- quit nvim
-vim.keymap.set("n", "<leader>Q", function()
+map("n", "<leader>Q", function()
     vim.cmd("wqa!")
 end)
 
 -- make Y behave like C and D - copy text until end of line
-vim.keymap.set("n", "Y", "yg_")
+map("n", "Y", "yg_")
 
 -- delete buffer
-vim.keymap.set("n", "<leader>db", "<cmd>bdelete<CR>")
+map("n", "<leader>db", "<cmd>bdelete<CR>")
 
 --jump up and down a page and center cursor
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
 
 -- center next and previous search results
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
 
 -- moves highlighted lines up (K) or down (J) in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
 
 -- move lines up or down in normal, insert, and visual modes
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
-vim.keymap.set("i", "<C-j>", "<Esc>:m .+1<CR>==gi")
-vim.keymap.set("i", "<C-k>", "<Esc>:m .-2<CR>==gi")
-vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
+map("n", "<A-j>", ":m .+1<CR>==")
+map("n", "<A-k>", ":m .-2<CR>==")
+map("i", "<C-j>", "<Esc>:m .+1<CR>==gi")
+map("i", "<C-k>", "<Esc>:m .-2<CR>==gi")
+map("v", "<C-j>", ":m '>+1<CR>gv=gv")
+map("v", "<C-k>", ":m '<-2<CR>gv=gv")
 
 -- helps you change all occurrences of the word the cursor is on
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -7,6 +7,9 @@ return {
         map("n", "<leader>Gl", "<cmd>Git log<CR>")
         map("n", "<leader>Gs", "<cmd>Git status<CR>")
         map("n", "<leader>Gd", "<cmd>Git diff<CR>")
-        map("n", "<leader>D", ":Git<CR><C-w><C-o>:bd<CR>")
+        map("n", "<leader>Gw", "<cmd>Gwrite<CR>")
+        map("n", "<leader>Gc", "<cmd>Git commit<CR>")
+        map("n", "<leader>Gm", "<cmd>Gwrite | :G commit<CR>")
+        map("n", "<leader>Gp", "<cmd>Git push<CR>")
     end,
 }
