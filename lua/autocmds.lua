285bf9f4d350226bdf43ffdd71b96da3c06276ca
Christian Gleitze <connectingdotscoding@gmail.com>
2025-11-24 16:57:00 +0100

    remove open-at-last-location autocmd

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,17 +1,5 @@
 local autocmd = vim.api.nvim_create_autocmd
 
-autocmd("BufReadPost", {
-    group = vim.api.nvim_create_augroup("cg/last_location", { clear = true }),
-    desc = "Go to the last location when opening a buffer",
-    callback = function(args)
-        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
-        local line_count = vim.api.nvim_buf_line_count(args.buf)
-        if mark[1] > 0 and mark[1] <= line_count then
-            vim.cmd('normal! g`"zz')
-        end
-    end,
-})
-
 autocmd("BufWritePre", {
     group = vim.api.nvim_create_augroup("cg/whitespace", {}),
     pattern = "*",
