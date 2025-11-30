b434b956db0782e07b608c0a9c80e431a8efe84a
Christian Gleitze <connectingdotscoding@gmail.com>
2025-11-30 12:13:59 +0100

    set termguicolors to true, del explaining remarks

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,35 +1,36 @@
 local o = vim.opt
 
--- General =====================
-o.backup = false -- don't store backup
-o.hidden = true -- load hidden buffers in memory
-o.mouse = "a" -- enable mouse
-o.nu = true -- set absolute [nu]mber for current line
-o.rnu = true -- [r]elative [nu]mbers for all other lines
-o.scrolloff = 10 -- always leave this number of lines above and below the cursor
-o.swapfile = false -- don't create swapfile
-o.undofile = true -- enable persistent undo
-o.linebreak = true -- break words, not letters at end of line
+-- General
+o.backup = false
+o.guicursor = ""
+o.hidden = true
+o.linebreak = true
+o.mouse = "a"
+o.nu = true
+o.rnu = true
+o.scrolloff = 10
+o.swapfile = false
+o.termguicolors = true
+o.undofile = true
 
--- update times and timeouts; important for performance
-o.updatetime = 300
+-- important for performance
 o.timeoutlen = 500
 o.ttimeoutlen = 10
+o.updatetime = 300
 
--- Editing =====================
-o.autoindent = true -- use auto indent
-o.incsearch = true -- incremental search
-o.hlsearch = true -- highlight search
-o.expandtab = true -- tab key inserts space characters
-o.tabstop = 4 -- insert 2 spaces for a tab
-o.shiftwidth = 4 -- use this number of spaces for indentation
-o.virtualedit = "block" -- allow going past the end of line in visual block mode
+-- Editing
+o.autoindent = true
+o.expandtab = true
+o.hlsearch = true
+o.incsearch = true
+o.shiftwidth = 4
+o.tabstop = 4
+o.virtualedit = "block"
 
--- UI ==========================
-o.cursorline = false -- no visual cursor line
--- o.guicursor = "n-v-ve-o-r-c-cr-sm:block-blinkwait175,i-ci:ver25-blinkwait175"
-o.cmdheight = 1 -- more space in the neovim command line to display messages
-o.signcolumn = "yes" -- always show signcolumn
-o.splitbelow = true -- horizontal splits will be below
-o.splitright = true -- vertical splits will be to the right
-o.winborder = "single" -- global border style for all popup menus
+-- UI
+o.cursorline = false
+o.cmdheight = 1
+o.signcolumn = "yes"
+o.splitbelow = true
+o.splitright = true
+o.winborder = "single"
