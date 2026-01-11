584f9879309e5262fe87dd5a7b55457b1e449ca8
Christian Gleitze <connectingdotscoding@gmail.com>
2026-01-11 01:28:52 +0100

    add more built-in stuff to disable

--- init.lua
+++ init.lua
@@ -6,6 +6,8 @@ g.mapleader = " "
 -- disable built-in stuff I don't use
 g.loaded_2html_plugin = 1
 g.loaded_gzip = 1
+g.loaded_getscript = 1
+g.loaded_getscriptPlugin = 1
 g.loaded_logiPat = 1
 g.loaded_matchit = 1
 g.loaded_matchparen = 1
@@ -14,6 +16,7 @@ g.loaded_netrwFileHandlers = 1
 g.loaded_netrwPlugin = 1
 g.loaded_netrwSettings = 1
 g.loaded_rplugin = 1
+g.loaded_rrhelper = 1
 g.loaded_spec = 1
 g.loaded_spellfile_plugin = 1
 g.loaded_tar = 1
