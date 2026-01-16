1cef7840fbe71560eb56451afa54f7994fc13916
Christian Gleitze <connectingdotscoding@gmail.com>
2026-01-16 15:57:15 +0100

    edit vim-fugitive keymap

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -6,7 +6,7 @@ return {
         map('n', '<leader>Gb', '<cmd>Git blame<CR>')
         map('n', '<leader>Gl', '<cmd>Git log<CR>')
         map('n', '<leader>Gd', '<cmd>Git diff<CR>')
-        map('n', '<leader>Gm', '<cmd>Gwrite | :G commit<CR>')
+        map('n', '<leader>Gw', '<cmd>Gwrite | :G commit<CR>')
         map('n', '<leader>Gp', '<cmd>Git push<CR>')
     end,
 }
