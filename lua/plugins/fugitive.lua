ce097fbd960278da4aaaa4042f402487148bda3f
Christian Gleitze <connectingdotscoding@gmail.com>
2026-01-16 01:31:57 +0100

    remove unused vim-fugitive keymaps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -5,10 +5,7 @@ return {
         map('n', '<leader>Gi', '<cmd>Git <CR>')
         map('n', '<leader>Gb', '<cmd>Git blame<CR>')
         map('n', '<leader>Gl', '<cmd>Git log<CR>')
-        map('n', '<leader>Gs', '<cmd>Git status<CR>')
         map('n', '<leader>Gd', '<cmd>Git diff<CR>')
-        map('n', '<leader>Gw', '<cmd>Gwrite<CR>')
-        map('n', '<leader>Gc', '<cmd>Git commit<CR>')
         map('n', '<leader>Gm', '<cmd>Gwrite | :G commit<CR>')
         map('n', '<leader>Gp', '<cmd>Git push<CR>')
     end,
