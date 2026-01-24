e3aa6abb06e34a88a79a70e32e8d96307dbb1bb3
Christian Gleitze <connectingdotscoding@gmail.com>
2026-01-24 01:16:25 +0100

    add live markdown preview plugin

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,17 +1,38 @@
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    ft = 'markdown',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {
-        link = {
-            enabled = false,
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        ft = 'markdown',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {
+            link = {
+                enabled = false, -- inline link icon rendering
+                footnote = {
+                    enabled = true,
+                    superscript = true,
+                },
+            },
+            keymaps = {
+                vim.keymap.set('n', '<leader>Md', function()
+                    require('render-markdown').buf_toggle()
+                end),
+            },
         },
-        keymaps = {
-            vim.keymap.set('n', '<leader>Md', function()
-                require('render-markdown').buf_toggle()
-            end),
+    },
+    {
+        'iamcco/markdown-preview.nvim',
+        ft = 'markdown',
+        build = function()
+            require('lazy').load({ plugins = { 'markdown-preview.nvim' } })
+            vim.fn['mkdp#util#install']()
+        end,
+        keys = {
+            {
+                '<leader>Mp',
+                ft = 'markdown',
+                '<cmd>MarkdownPreviewToggle<cr>',
+            },
         },
     },
 }
