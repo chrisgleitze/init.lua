7cb7af8205e159a0be77a229862ed756896bde2a
Christian Gleitze <connectingdotscoding@gmail.com>
2026-03-29 17:27:16 +0200

    edit in stylelint config

--- lsp/stylelint_lsp.lua
+++ lsp/stylelint_lsp.lua
@@ -4,8 +4,26 @@
 ---@type vim.lsp.Config
 return {
     cmd = { 'stylelint-language-server', '--stdio' },
-    filetypes = { 'astro', 'css', 'html', 'less', 'scss', 'vue' },
-    root_markers = { '.stylelintrc', '.stylelintrc.js', '.stylelintrc.json', 'stylelint.config.js' },
+    filetypes = {
+        'astro',
+        'css',
+        'html',
+        'less',
+        'scss',
+        'vue',
+    },
+    root_markers = {
+        '.stylelintrc',
+        '.stylelintrc.mjs',
+        '.stylelintrc.cjs',
+        '.stylelintrc.js',
+        '.stylelintrc.json',
+        '.stylelintrc.yaml',
+        '.stylelintrc.yml',
+        'stylelint.config.mjs',
+        'stylelint.config.cjs',
+        'stylelint.config.js',
+    },
     settings = {
         stylelintplus = {
             -- lint on save instead of on type.
