a869a3efbe520f7395c140458f602ae0e8e056ad
Christian Gleitze <connectingdotscoding@gmail.com>
2026-04-20 19:39:09 +0200

    rework treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,58 @@
 return {
     'nvim-treesitter/nvim-treesitter',
-    event = { 'BufReadPre', 'BufNewFile' },
     lazy = false,
-    branch = 'main',
     build = ':TSUpdate',
-    opts = {
-        ensure_installed = 'all',
-        auto_install = true,
-        sync_install = true,
-        highlight = { enable = true },
-    },
+    init = function()
+        local parsers = {
+            'bash',
+            'c',
+            'cpp',
+            'gitcommit',
+            'gitignore',
+            'go',
+            'graphql',
+            'html',
+            'java',
+            'javascript',
+            'json',
+            'json5',
+            'lua',
+            'markdown',
+            'markdown_inline',
+            'php',
+            'python',
+            'query',
+            'r',
+            'regex',
+            'rust',
+            'scss',
+            'toml',
+            'tsx',
+            'typescript',
+            'vim',
+            'vimdoc',
+            'yaml',
+        }
+
+        local group = vim.api.nvim_create_augroup('cg/treesitter', { clear = true })
+        vim.api.nvim_create_autocmd({ 'BufEnter', 'FileType' }, {
+            group = group,
+            callback = function()
+                if vim.bo.buftype ~= '' then
+                    return
+                end
+
+                pcall(vim.treesitter.start, 0)
+            end,
+        })
+
+        vim.api.nvim_create_autocmd('User', {
+            group = group,
+            pattern = 'VeryLazy',
+            once = true,
+            callback = function()
+                require('nvim-treesitter').install(parsers)
+            end,
+        })
+    end,
 }
