8da7362e001c66a61d0633300d171ca145401d90
Christian Gleitze <connectingdotscoding@gmail.com>
2026-05-03 13:57:59 +0200

    fix(neo-tree): avoid broad modified event autocmd

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -16,6 +16,20 @@ return {
             width = 35,
         },
     },
+    config = function(_, opts)
+        local events = require('neo-tree.events')
+        local define_autocmd_event = events.define_autocmd_event
+
+        events.define_autocmd_event = function(event_name, autocmds, ...)
+            if event_name == events.VIM_BUFFER_MODIFIED_SET then
+                autocmds = { 'OptionSet modified' }
+            end
+
+            return define_autocmd_event(event_name, autocmds, ...)
+        end
+
+        require('neo-tree').setup(opts)
+    end,
     keys = {
         vim.keymap.set('n', '<leader>t', '<Cmd>Neotree toggle<CR>'),
         vim.keymap.set('n', '<leader>T', '<Cmd>Neotree position=current toggle<CR>'),
