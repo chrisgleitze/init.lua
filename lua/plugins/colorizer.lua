d7f7ad72a00db36742f4e59ae325a0f9230338f6
Christian Gleitze <connectingdotscoding@gmail.com>
2026-05-09 21:10:09 +0200

    change options to opts in colorizer and splitjoin

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -2,7 +2,7 @@
 return {
     'catgoose/nvim-colorizer.lua',
     ft = { 'css', 'scss', 'html', 'javascript', 'typescript', 'json', 'jsonc', 'lua', 'yaml' },
-    options = {
+    opts = {
         user_default_options = {
             tailwind = true,
             css = true,
