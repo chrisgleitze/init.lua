346a74a1dbc5f490ccfb035355cd54f3c0b7866a
Christian Gleitze <connectingdotscoding@gmail.com>
2026-05-09 14:43:01 +0200

    remove lazy loading from vim-visual-multi

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -2,7 +2,6 @@ return {
     'mg979/vim-visual-multi',
     branch = 'master',
     event = { 'BufReadPre', 'BufNewFile' },
-    lazy = false,
     config = function()
         vim.keymap.set('n', '<C-x>', '<Plug>(VM-Find-Under)')
     end,
