0710c99553a1d1190b4556812b3c7c7b4504bdfa
Christian Gleitze <connectingdotscoding@gmail.com>
2026-05-09 15:07:45 +0200

    edit fugitive config

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,17 +1,30 @@
 return {
     'tpope/vim-fugitive',
-    dependencies = { 'junegunn/gv.vim' },
-    config = function()
-        local map = vim.keymap.set
-        map('n', '<leader>Gi', '<cmd>Git <cr>')
-        map('n', '<leader>Gb', '<cmd>Git blame<cr>')
-        map('n', '<leader>Gl', '<cmd>Git log<cr>')
-        map('n', '<leader>Gd', '<cmd>Git diff<cr>')
-        map('n', '<leader>Gw', '<cmd>Gwrite | :G commit<cr>')
-        map('n', '<leader>Gp', '<cmd>Git push<cr>')
+    cmd = {
+        'Git',
+        'Git blame',
+        'Git log',
+        'Git diff',
+        'Gwrite',
+        'Git commit',
+        'Git push',
+    },
+    dependencies = {
+        {
+            'junegunn/gv.vim',
+            cmd = 'GV',
+        },
+    },
+    keys = {
+        { '<leader>Gi', '<cmd>Git<cr>' },
+        { '<leader>Gb', '<cmd>Git blame<cr>' },
+        { '<leader>Gl', '<cmd>Git log<cr>' },
+        { '<leader>Gd', '<cmd>Git diff<cr>' },
+        { '<leader>Gw', '<cmd>Gwrite | Git commit<cr>' },
+        { '<leader>Gp', '<cmd>Git push<cr>' },
 
-        map('n', '<leader>GVV', '<cmd>GV<cr>')
-        map('n', '<leader>GV!', '<cmd>GV!<cr>')
-        map('n', '<leader>GV?', '<cmd>GV?<cr>')
-    end,
+        { '<leader>GVV', '<cmd>GV<cr>' },
+        { '<leader>GV!', '<cmd>GV!<cr>' },
+        { '<leader>GV?', '<cmd>GV?<cr>' },
+    },
 }
