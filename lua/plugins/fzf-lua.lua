8de7c25d8cf4d7d1482b1db3033296c9e18477eb
Christian Gleitze <connectingdotscoding@gmail.com>
2026-05-09 15:08:00 +0200

    make fzf-lua load lazy

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,7 +1,6 @@
 -- File Picker
 return {
     'ibhagwan/fzf-lua',
-    lazy = false,
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         require('fzf-lua').setup({
