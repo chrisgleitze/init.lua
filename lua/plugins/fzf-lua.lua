25561499d9dd766fa66b532b66875b7fbcc4e3c7
Christian Gleitze <connectingdotscoding@gmail.com>
2026-05-09 23:58:27 +0200

    add FzfLua cmd

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,6 +1,7 @@
 -- File Picker
 return {
     'ibhagwan/fzf-lua',
+    cmd = 'FzfLua',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         require('fzf-lua').setup({
