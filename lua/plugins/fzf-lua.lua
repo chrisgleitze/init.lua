ee48e647a687a50ae6fe1aab2bd726a3c3d37310
Christian Gleitze <connectingdotscoding@gmail.com>
2026-06-15 11:39:51 +0200

    fzf-lua: add ui_select

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -5,6 +5,7 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         require('fzf-lua').setup({
+            ui_select = true,
             fzf_colors = true,
             fzf_opts = {
                 ['--no-scrollbar'] = false,
