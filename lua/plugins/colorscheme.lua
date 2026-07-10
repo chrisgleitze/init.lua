193afa100954ac45161d0929c651cb10b098b168
Christian Gleitze <connectingdotscoding@gmail.com>
2026-07-10 14:29:07 +0200

    change to rosepine colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,21 +1,21 @@
 return {
-    -- 'rose-pine/neovim',
-    -- name = 'rose-pine',
-    -- lazy = false,
-    -- priority = 1000,
-    -- config = function()
-    --     require('rose-pine').setup({
-    --         dark_variant = 'main',
-    --         styles = {
-    --             italic = false,
-    --         },
-    --         groups = {
-    --             background = '#0d0a0a',
-    --         },
-    --     })
-    --
-    --     vim.cmd('colorscheme rose-pine-main')
-    -- end,
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require('rose-pine').setup({
+            dark_variant = 'main',
+            styles = {
+                italic = false,
+            },
+            groups = {
+                background = '#0d0a0a',
+            },
+        })
+
+        vim.cmd('colorscheme rose-pine-main')
+    end,
 
     -- 'gruvbox-community/gruvbox',
     -- lazy = false,
@@ -25,14 +25,14 @@ return {
     --     vim.cmd('colorscheme gruvbox')
     -- end,
 
-    'vague-theme/vague.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require('vague').setup({
-            bold = false,
-            italic = false,
-        })
-        vim.cmd.colorscheme('vague')
-    end,
+    -- 'vague-theme/vague.nvim',
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --     require('vague').setup({
+    --         bold = false,
+    --         italic = false,
+    --     })
+    --     vim.cmd.colorscheme('vague')
+    -- end,
 }
