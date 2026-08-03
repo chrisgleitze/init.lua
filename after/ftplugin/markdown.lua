1cfeafc22b83cf4d55fc35fd9d7fa85d7b45e3a0
Christian Gleitze <connectingdotscoding@gmail.com>
2026-08-03 22:56:13 +0200

    add md options to markdown ftplugin

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,1 +1,9 @@
 vim.opt_local.autoindent = false
+vim.opt_local.spell = true
+vim.opt_local.spelllang = { 'de_de', 'en_us' }
+vim.opt_local.spellcapcheck = ''
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+vim.opt_local.breakindent = true
+
+vim.keymap.set('n', 'zs', '1z=', { buffer = true })
