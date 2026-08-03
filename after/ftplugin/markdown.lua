9a05a3e3e343666a9cc4890a110362248dfbc576
Christian Gleitze <connectingdotscoding@gmail.com>
2026-08-04 00:37:47 +0200

    change markdown options

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,8 +2,8 @@ vim.opt_local.autoindent = false
 vim.opt_local.spell = true
 vim.opt_local.spelllang = { 'de_de', 'en_us' }
 vim.opt_local.spellcapcheck = ''
-vim.opt_local.wrap = true
-vim.opt_local.linebreak = true
+vim.opt_local.wrap = false
+vim.opt_local.linebreak = false
 vim.opt_local.breakindent = true
 
 vim.keymap.set('n', 'zs', '1z=', { buffer = true })
