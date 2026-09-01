7be25d82928bc7b5e62b7176364182dd85c123a2
Christian Gleitze <connectingdotscoding@gmail.com>
2026-09-02 00:28:33 +0200

    neotree: remove redundant stuff

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -16,20 +16,6 @@ return {
             width = 35,
         },
     },
-    config = function(_, opts)
-        local events = require('neo-tree.events')
-        local define_autocmd_event = events.define_autocmd_event
-
-        events.define_autocmd_event = function(event_name, autocmds, ...)
-            if event_name == events.VIM_BUFFER_MODIFIED_SET then
-                autocmds = { 'OptionSet modified' }
-            end
-
-            return define_autocmd_event(event_name, autocmds, ...)
-        end
-
-        require('neo-tree').setup(opts)
-    end,
     keys = {
         { '<leader>t', '<cmd>Neotree toggle<cr>' },
         { '<leader>T', '<cmd>Neotree position=current toggle<cr>' },
