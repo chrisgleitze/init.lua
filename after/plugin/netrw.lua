a47260c3593b4e79981ea9966260778e10ba557d
Christian Gleitze <connectingdotscoding@gmail.com>
2026-09-02 10:31:46 +0200

    remove netrw again

--- after/plugin/netrw.lua
+++
@@ -1,7 +0,0 @@
--- stylua: ignore start
-vim.g.netrw_altv = 1          -- split to the right
-vim.g.netrw_banner = 0        -- suppress the banner text
-vim.g.netrw_browse_split = 4  -- open files in the previous window
-vim.g.netrw_liststyle = 3     -- tree style listing
-vim.g.netrw_winsize = 50      -- initial size of new windows
--- stylua: ignore end
