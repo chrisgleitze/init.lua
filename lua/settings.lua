e3078a873156a83d50f987ba8f2317afe732c254
Christian Gleitze <connectingdotscoding@gmail.com>
2026-09-05 00:00:31 +0200

    settings.lua: now leaner and re-organized

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,17 +1,7 @@
 local o = vim.opt
 
--- general
-o.backup = false
-o.clipboard = ''
-o.guicursor = ''
-o.hidden = true
-o.linebreak = true
-o.mouse = 'a'
-o.number = true
-o.relativenumber = true
-o.scrolloff = 10
+-- files
 o.swapfile = false
-o.termguicolors = true
 o.undofile = true
 o.shada = {
     '!',
@@ -31,7 +21,36 @@ o.sessionoptions = {
     'winsize',
 }
 
--- wsl clipboard provider
+-- timing
+o.timeoutlen = 400 -- wait for mapped key sequences
+o.updatetime = 300 -- CursorHold, lsp highlights, gitsigns
+
+-- editing
+o.expandtab = true
+o.shiftwidth = 4
+o.tabstop = 4
+o.virtualedit = 'block'
+
+-- search
+o.ignorecase = true
+
+-- UI
+o.cursorline = false
+o.guicursor = ''
+o.linebreak = true
+o.mouse = 'a'
+o.number = true
+o.relativenumber = true
+o.scrolloff = 10
+o.signcolumn = 'yes'
+o.splitbelow = true
+o.splitright = true
+o.termguicolors = true
+o.statusline = " %{get(b:,'gitsigns_head','')} %f%m%r%=%{&filetype} %l:%c "
+o.winborder = 'single'
+o.pumborder = 'single'
+
+-- clipboard: yanks stay in vim's registers, "+ and "* go through this wsl provider
 local wsl_copy = { 'sh', '-c', 'iconv -f UTF-8 -t UTF-16LE | /mnt/c/Windows/System32/clip.exe' }
 local wsl_paste = {
     '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe',
@@ -52,31 +71,3 @@ vim.g.clipboard = {
     },
     cache_enabled = 0,
 }
-
--- important for performance
-o.timeout = true -- don't wait indefinitely for mapped keys
-o.timeoutlen = 400
-o.ttimeoutlen = 50 -- timeout for key codes
-o.updatetime = 300
-
--- editing
-o.autoindent = true
-o.autoread = true
-o.expandtab = true
-o.hlsearch = true
-o.incsearch = true
-o.ignorecase = true
-o.shiftwidth = 4
-o.tabstop = 4
-o.virtualedit = 'block'
-
--- UI
-o.cursorline = false
-o.cmdheight = 1
-o.signcolumn = 'yes'
-o.splitbelow = true
-o.splitright = true
-
-o.statusline = " %{get(b:,'gitsigns_head','')} %f%m%r%=%{&filetype} %l:%c "
-o.winborder = 'single'
-o.pumborder = 'single'
